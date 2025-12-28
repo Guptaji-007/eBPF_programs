9c83d7c8c64765234f1657699a5d848c88e41087
Ankit Gupta <guptaankitvinodkumar@gmail.com>
2025-12-28 11:48:33 +0530

    Updated Readme

--- ebpf/hello.bpf.c
+++ ebpf/hello.bpf.c
@@ -8,6 +8,7 @@ SEC("tracepoint/syscalls/sys_enter_execve")
 int hello_execve(void *ctx)
 {
     bpf_printk("Hello from eBPF!\n");
+    bpf_printk("This is the first program...TESTING!\n");
     return 0;
 }
 
